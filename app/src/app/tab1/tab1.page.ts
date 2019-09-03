3032015162c40f85e7ccc921170fd245a245da04
Anghelo Rodriguez <acherfun99@gmail.com>
2019-09-03 14:54:31 -0400

    crud para administradores

--- app/src/app/tab1/tab1.page.ts
+++ app/src/app/tab1/tab1.page.ts
@@ -4,6 +4,7 @@ import { UsuarioService } from '../services/usuario.service';
 import { LoginService } from '../services/login.service';
 import { Login } from '../models/login';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -15,28 +16,22 @@ export class Tab1Page implements OnInit{
   registroForm:FormGroup
   listUsersAdmins:any[] = [];
   userAux:Usuario;
-  loginaux:Login
-  estado:boolean
+  loginaux:Login;
+  estado:boolean;
   usuario:string;
   contracena:string;
+  id:string;
+  estadoUpdate:boolean;
   constructor(
     private userService:UsuarioService,
     private userLogin:LoginService,
+    private alertController: AlertController
     //private formBuilder: FormBuilder,
   ) 
   {
     //TODO CONSTRUCTOR
-    this.estado=false;
-    this.userAux=new Usuario;
-    this.loginaux=new Login;
-    this.userAux.email=" ";
-    this.contracena=" qwe"
-    this.userAux.tipo_usuario="Administrador";
-    
-    //this.userAux.login={usuario:"",contrasena:""};
-    // this.usuario=this.userAux.login.usuario;
-    // this.contracena=this.userAux.login.contrasena;
-    
+    this.initializeVariables();
+  
 
   }
 
@@ -46,15 +41,43 @@ export class Tab1Page implements OnInit{
 
   initializeVariables(){
     this.estado=false;
+    this.estadoUpdate=false;
     this.userAux;
     // this.loginaux=new Login;
-    this.userAux.email="";
+    this.loginaux={email:"",contrasena:""};    
+    this.userAux={email:"",nombre:"",apellidos:"",tipo_usuario:"",foto:""};
     this.contracena=""
-    this.userAux.tipo_usuario="Administrador";
+    this.userAux.tipo_usuario="administrador";
   }
+
+  async presentAlertConfirm(id:string) {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: '<strong>Esta seguro de eliminar el dato?</strong>',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.deleteUserInDataBase(id);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
 
   getUsersAdmins()
   {  
+    this.listUsersAdmins=[]
     let list:any;
     this.userService.listar().subscribe(res =>{
       list = res.map(e =>{
@@ -69,20 +92,33 @@ export class Tab1Page implements OnInit{
       });
       console.log(this.listUsersAdmins);
     })
-  }
+  }  
 
-  deleteUserInDataBase(id:string,user:Usuario){
-    this.userService.actualizar(id,user).then(data =>{
-      if(data != null){
-        console.log("datos borrrados");
-      }
+  deleteUserInDataBase(id:string){
+    this.userService.eliminar(id).then(data =>{
+     
+        this.getUsersAdmins();
+     
     })
-    console.log(user);
-
+    console.log(id);
   }
 
-  updateUserInDataBase(user:Usuario){
+  updateUserInDataBase(){       
+   
+      this.userService.actualizar(this.id,this.userAux).then(data =>{
+        this.initializeVariables();
+        this.getUsersAdmins()
+      });
+  }
 
+  goToUpdateStep(user:any){
+    this.estado=true;
+    this.id=user.id;
+    this.userAux=user;
+    this.estadoUpdate=true;
+    
+    console.log(this.id);
+    console.log(this.userAux);
   }
 
   enabledToCreateUser(){
@@ -91,20 +127,22 @@ export class Tab1Page implements OnInit{
 
   disabledToCreateUser(){
     this.estado=false;
+    this.initializeVariables();
   }
 
   getUserToSaveInDataBase(){
-    // this.loginaux.email=this.userAux.email;
-    // if(this.loginaux.contrasena != "" || this.loginaux.email != "")
-    //   this.userLogin.register(this.loginaux.email,this.loginaux.contrasena).then((data) =>{
+     this.loginaux.email=this.userAux.email;
+     if(this.loginaux.contrasena != "" || this.loginaux.email != "")
+       this.userLogin.register(this.loginaux.email,this.loginaux.contrasena).then((data) =>{
       this.userAux.foto="";
        console.log(this.userAux);
            this.userService.crear(this.userAux).then(data =>{
              this.initializeVariables();
+             this.getUsersAdmins()
            })
      
         
-   //   });
+     });
     
   }
 
