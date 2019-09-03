6fcf3aa61e597fc602f30a975cc0089016116c0c
Anghelo Rodriguez Apaza <acherfun99@gmail.com>
2019-09-03 17:37:51 -0400

    añadiendo cambios de diseño en boton salir cerrar sesion

--- app/src/app/tabs/tabs.page.ts
+++ app/src/app/tabs/tabs.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { LoginService } from '../services/login.service';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { PopoverController } from '@ionic/angular';
+import { PopoverPage } from '../popovers/popover/popover.page';
 
 @Component({
   selector: 'app-tabs',
@@ -12,12 +14,22 @@ export class TabsPage {
 
   constructor(private loginService: LoginService,
     private route: Router,
-    private storage: Storage) { }
+    private storage: Storage,
+    public popoverController: PopoverController) { }
 
   cerrarSesion() {
     this.loginService.logout().then(() => {
       this.storage.clear();
       this.route.navigateByUrl('login');
     })
+  }
+  async presentPopover(ev: any) {
+    const popover = await this.popoverController.create({
+      component: PopoverPage,
+      event: ev,
+      translucent: true,
+      mode:"ios"
+    });
+    return await popover.present();
   }
 }
